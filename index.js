38647a24574a790f2dc3af23d5d4d029068e27ac
Syed Maqthyar <maqhti@gmail.com>
2020-07-02 14:22:38 +0530

    fetch data from api

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 const express =require('express');
-const router =express.Router();
+const app =express();
 const fetch =require('node-fetch');
-
+const http =require('http');
 const bodyParser= require('body-parser');
 
-router.use(bodyParser.json());
+app.use(bodyParser.json());
+
+let lon=75.698975,lat=13.946204;
+let key="5cfaa3fc91d59befb10d31a841f0ece5";
 
 
-fetch("http://api.openweathermap.org/data/2.5/weather?lat=13.946204&lon=75.698975&units=metric&appid=5cfaa3fc91d59befb10d31a841f0ece5")
-    .then((resp)=>resp.json())
-    .then((resp) =>
-    {
-        console.log(resp);
-    })
+app.get('/',(req,res,next) =>{
+   fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.lon}&units=metric&appid=${key}`)
+       .then((resp)=>
+       {
+           if(resp.ok)
+               return resp;
+           else
+           {
+               console.log(resp);
+               var err =new Error("Error");
+               err.statusText=resp.statusText;
+               throw (err);
+           }
+       },err =>{throw err})
+        .then((resp)=>resp.json())
+        .then((resp) =>
+        {
+            if(resp.cod===200)
+            {
+                let date =new Date(resp.sys.sunset*1000)
+                console.log("Time is "+date.getHours()+" : "+date.getMinutes()+" : "+date.getSeconds());
+                res.statusCode=200;
+                res.setHeader('Content-Type',"application/json");
+                return res.json(resp);
+            }
+            else
+            {
+                res.statusCode=501;
+                console.log(resp);
+                res.end();
+            }
+        })
+           .catch(err=>{
+               res.statusCode=200;
+                res.setHeader('Content-Type',"application/json");
+                return res.json(err);
+           })
+})
+
+var port = (process.env.PORT || '3000');
+app.set('port',port);
+
+let server= http.createServer(app);
+server.listen(port, () =>
+{
+    console.log("Listening on port ",port);
+})
+
+
 
 
