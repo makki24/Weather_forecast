7a8af5b25b50b96e1e12c5d0dc3983d780d65b9d
Syed Maqthyar <maqhti@gmail.com>
2020-07-06 22:33:58 +0530

    Weather Information display

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 
  let lat,lon;
- let weath_icon ="http://openweathermap.org/img/wn/"
+ let weath_icon ="http://openweathermap.org/img/wn/";
+ let flag_icon= "http://openweathermap.org/images/flags/"
     function getLocation()
     {
         if(navigator.geolocation)
@@ -42,12 +43,20 @@
                         {
                             console.log(resp);
                             let path=weath_icon+resp.weather[0].icon+"@2x.png";
+                            let flg_path=flag_icon+resp.sys.country.toLowerCase()+".png";
                             let sunrise=new Date(resp.sys.sunrise*1000);
-                            console.log(path);
+                            let sunset =new Date(resp.sys.sunset*1000);
                             document.getElementById('icon').setAttribute("src",path);
+                            document.getElementById('desc').innerHTML =
+                                resp.weather[0].description.charAt(0).toUpperCase() + resp.weather[0].description.slice(1);
                             document.getElementById('temp').innerHTML =" : "+resp.main.temp+"<sup>o</sup> cel";
                             document.getElementById('rise').innerHTML =" : "+sunrise.getHours()+":"+sunrise.getMinutes()+":" +
                                 ""+sunrise.getSeconds();
+                            document.getElementById('set').innerHTML =" : "+sunset.getHours()%12+":"+sunset.getMinutes()+":" +
+                                ""+sunset.getSeconds();
+                            document.getElementById('city').innerHTML=" : "+resp.name;
+                            document.getElementById('country').innerHTML=" : "+resp.sys.country;
+                            document.getElementById('flg').setAttribute("src",flg_path);
                         }
                         else
                         {
